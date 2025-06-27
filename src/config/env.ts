5b6e784fdd7c61d57469f74cd4244ee562589854
Dirosaki <diegosgomes27@gmail.com>
2025-06-27 02:46:16 -0300

    chore: config env schema and api wrapper

---
+++ src/config/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod/v4'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables:',
+    z.treeifyError(parsedEnv.error).errors,
+  )
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = parsedEnv.data
