94bdb284ad1d5ef65f5b5b76c35dfe68e2865402
Dirosaki <diegosgomes27@gmail.com>
2025-06-28 03:57:35 -0300

    feat: add generateStaticParams in product details page

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -30,6 +30,13 @@ export async function generateMetadata({
   }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map(({ slug }) => ({ slug }))
+}
+
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
 
